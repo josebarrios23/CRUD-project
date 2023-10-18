77b647d0129b1dbaafa06d366ec2a783ec9c8a63
Jose Barrios <josebarrios@pursuit.org>
2023-10-17 22:31:19 -0400

    made edits to previous function and finished 2nd

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const { readJSONFile, writeJSONFile } = require('./src/helpers')
 const cart = readJSONFile('./data', 'cart.json')
 const hardwareItems = readJSONFile('./data/', 'hardwareItems.json')
-const { create, index, show, destroy, updateInventory, updateCart, total, cancel, checkStock } = require('./src/controller')
+const { add, showCart, showItem, destroy, updateInventory, updateCart, total, cancel, checkStock } = require('./src/controller')
 const inform = console.log
 
 function run() {
   const action = process.argv[2]
   const itemName = process.argv[3]
 
-
   let writeToFile = false
   let updatedItems = []
 
   switch (action) {
+
+    case 'showCart':
+      const seeCart = showCart(cart)
+      inform(seeCart)
+      break;
+
+    case "add":
+      updatedItems = add(cart, itemName)
+      writeToFile = true
+      break;
     default:
       inform('There was an error.')
   }
 
   if (writeToFile) {
     writeJSONFile('./data', 'cart.json', updatedItems)
-  }
-
-  if (writeToFile2) {
-    writeJSONFile('./data', 'hardwareItems.json', updatedItems)
   }
 }
 
--- src/controller.js
+++ src/controller.js
@@ -3,11 +3,74 @@ const hardwareItems = require('../data/hardwareItems.json')
 
 const inform = console.log
 
-function create() {}
+function add(cart, itemName) {
+  for (const item of cart) {
+    if (item.name === itemName.toLowerCase()) {
+      item.amount++
+      inform("Item already in cart. Amount Increased")
+      let totalPrice = 0
+      let totalAmount = 0
+      cart.forEach((item) => {
+        totalPrice += item.priceInCents
+        totalAmount += item.amount
+    })
+    inform(`CART TOTAL: ${((totalPrice/100).toFixed(2))} - AMOUNT OF ITEMS: ${totalAmount}`)
+      return cart
+    }
+  }
+  for (const item of hardwareItems) {
+    if (item.name === itemName.toLowerCase()) {
+      inform("Adding unique ID to inventory item, adding to cart")
+      item.id = faker.string.alpha(7)
+      item.amount = 1
+      cart.push(item)
+      let totalPrice = 0
+      let totalAmount = 0
+      cart.forEach((item) => {
+        totalPrice += item.priceInCents
+        totalAmount += item.amount
+      })
+      inform(`CART TOTAL: ${((totalPrice/100).toFixed(2))} - AMOUNT OF ITEMS: ${totalAmount}`)
+      return cart
+    }
+  }
+  const newItem = {
+    name: itemName.toLowerCase(),
+    priceInCents: faker.number.int({ min: 5000, max: 29999 }),
+    buyOnline: faker.datatype.boolean(),
+    inStock: [
+      faker.datatype.boolean(),
+      faker.number.int({ max: 100 })
+    ],
+    id: faker.string.alpha(7),
+    amount: 1
+  }
+  if (!newItem.inStock[0]){
+    newItem.inStock.pop()
+    newItem.inStock.push(0)
+  }
+  cart.push(newItem)
+  inform("Item not in inventory. Special order created and added to cart")
+  let totalPrice = 0
+      let totalAmount = 0
+      cart.forEach((item) => {
+        totalPrice += item.priceInCents
+        totalAmount += item.amount
+      })
+      inform(`CART TOTAL: ${((totalPrice/100).toFixed(2))} - AMOUNT OF ITEMS: ${totalAmount}`)
+  return cart
+}
 
-function index() {}
+function showCart(cart) {
+  let total = 0
+  cart.forEach((item) => {
+    total += item.priceInCents
+  })
+  inform(`CART TOTAL: ${((total/100).toFixed(2))}`)
+  return cart.map((item) => item.name + ' ' + "$" + (item.priceInCents/ 100).toFixed(2)).join('\n')
+}
 
-function show() {}
+function showItem() {}
 
 function destroy() {}
 
@@ -19,12 +82,12 @@ function total() {}
 
 function cancel() {}
 
-function checkStock() {}
+function checkStock(){}
 
 module.exports = {
-  create,
-  index,
-  show,
+  add,
+  showCart,
+  showItem,
   destroy,
   updateInventory,
   updateCart,
